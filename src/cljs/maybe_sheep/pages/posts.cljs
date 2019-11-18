259708b88dcc127a684f80192cb88c85a93fe43e
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-11-18 15:54:26 -0500

    last update

--- src/cljs/maybe_sheep/pages/posts.cljs
+++ src/cljs/maybe_sheep/pages/posts.cljs
@@ -11,12 +11,12 @@
 
 (defn posts-page []
   (fn []
-    [:span.w-75.flex.flex-column.justify-around.overflow-scroll.content-center.items-center.bw2.b--black
+    [:span.w-75.flex.flex-column.justify-evenly.overflow-scroll.content-center.items-center.bw2.b--black
      [:h1.w5.tc.avenir "Maybe Posts"]
      (for [item content-list]
        (let [kw (first item)
              {:keys [title url prev]} (second item)]
          ^{:key kw}
-         [:div.w-100.flex.flex-row.justify-center
+         [:div.w-100.flex.flex-row.justify-center.ma2
           [:a.avenir.link.dim.navy.truncate {:href (path-for :post {:post-id url})
                                              :on-click #(swap! current-post assoc :current-post kw)} title]]))]))
