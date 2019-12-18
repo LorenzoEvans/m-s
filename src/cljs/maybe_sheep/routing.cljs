9ddc1c92b61407347195a380f8c69f0e35a1e596
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-18 16:11:28 -0500

    re-factoring routing based on conduit

--- src/cljs/maybe_sheep/routing.cljs
+++ src/cljs/maybe_sheep/routing.cljs
@@ -1,9 +1,20 @@
 (ns maybe-sheep.routing
   (:require [reitit.frontend :as r]
             [bidi.bidi :as bidi]
+            [re-frame.core :refer [dispatch]]
             [kibu/pushy :as pushy]
             [maybe-sheep.articles.article-content :refer [content-store]]))
 
+
+(def routes
+  ["/" {"" :home
+        "/posts" {[:slug] :article}}])
+
+(def history 
+  (let [dispatch #(dispatch [:set-active-page {:page (:handler %)
+                                               :slug (get-in % [:route-params :slug])}])
+        match #(bidi/match-route routes %)]
+        (pushy/pushy dispatch match)))
 ; (def router
 ;   (r/router
 ;    [["/" :index]
