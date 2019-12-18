7b438b0624e521442cd7441a6aa00cedfd21fd70
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-18 16:28:47 -0500

    Ooookay, calm down, the codes just getting hairy

--- src/cljs/maybe_sheep/subs.cljs
+++ src/cljs/maybe_sheep/subs.cljs
@@ -1,13 +1,23 @@
 (ns maybe-sheep.subs
     (:require [re-frame.core :refer [reg-sub subscribe]]))
+              ;import db
 
 (defn reverse-cmp 
     [a b]
     (compare a b))
 
-(reg-sub
-    :active-page 
+(reg-sub :articles
     (fn [db _]
         (->> (:articles db)
              (vals)
              (sort-by :epoch reverse-cmp))))
+
+(reg-sub :active-page
+    (fn [db _]
+        (:active-page db)))
+
+(reg-sub :articles-count
+    (fn [db _]
+        (:articles-count db)))
+             
+(reg-sub :)
