0d536eae967ddc2772fa2e6b59e78e692779fb0f
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-18 17:58:07 -0500

    Plugging onward

--- src/cljs/maybe_sheep/events.cljs
+++ src/cljs/maybe_sheep/events.cljs
@@ -6,3 +6,17 @@
 (reg-fx :set-url
     (fn [{:keys [url]}]
         (router/set-token! url)))
+
+(reg-event-fx :set-active-page
+    (fn-traced [{:keys [db]} [_ {:keys [page slug]}]]
+        (let [set-page (assoc db :active-page page)]
+            (case page
+                :home {:db set-page
+                       :dispatch [[:get-articles {:limit 10}]]}
+                :article {:db (assoc :set-page :active-artle slug)
+                          :dispatch [:get-articles]}))))
+
+(reg-event-db :set-active-article
+    (fn-traced [{:keys [db]} [_ slug]]
+        {:db (assoc db :active-article slug)}))
+
