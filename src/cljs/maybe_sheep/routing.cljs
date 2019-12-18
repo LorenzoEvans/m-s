ff5b307938cd829368770bbf68cc6c6763ea313e
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-18 16:17:43 -0500

    plz let this work lol

--- src/cljs/maybe_sheep/routing.cljs
+++ src/cljs/maybe_sheep/routing.cljs
@@ -7,14 +7,30 @@
 
 
 (def routes
+; Routes define what will be shown when certain path are hit. A route is a vector with a pattern to match, and a result for the match.
   ["/" {"" :home
         "/posts" {[:slug] :article}}])
 
 (def history 
+; We keep track of history so we can navigate back and forward, and pushy does that, by consuming the dispatch function
+; what happens on dispatch and match: what routes should we match.
   (let [dispatch #(dispatch [:set-active-page {:page (:handler %)
                                                :slug (get-in % [:route-params :slug])}])
         match #(bidi/match-route routes %)]
         (pushy/pushy dispatch match)))
+
+(defn- parse-url
+; bidi/match-route converts urls to data structures and check if the route exists in our route.
+  [url]
+  (bidi/match-route routes url))
+
+(defn start!
+  (pushy/start! history))
+
+(defn url-for (partial bidi/path-for routes))
+
+(defn set-token! [token]
+  (pushy/set-token! history token))
 ; (def router
 ;   (r/router
 ;    [["/" :index]
