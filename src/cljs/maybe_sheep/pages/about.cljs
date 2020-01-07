f2a5cdf9d708ad4536b47c1f76a99a58d8d9de5e
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-07 13:16:42 -0500

    test

--- src/cljs/maybe_sheep/pages/about.cljs
+++ src/cljs/maybe_sheep/pages/about.cljs
@@ -2,7 +2,7 @@
 
 (defn about-page []
   (fn [] [:span.w-75.bw2.b--near-black.overflow-scroll.flex.flex-column.justify-around
-          [:h1.f2.avenir.grow.ml1.f3-m.w-75.tc.pa2 "I'm Lorenzo Evans, a developer, musician, writer, thinker and creative."
+          [:h1.f2.avenir.grow.ml1.f3-m.w-75.tc.pa2 "I'm Lorenzo Evansss  asdasdssss, a developer, musician, writer, thinker and creative."
            [:h1.f4.avenir.hover-animate.hover-bg-dark-red.hover-navy.ml1.f3-m.pa2 "I recently finished the "
             [:span.hover-bg-dark-red.hover-animate.hover-washed-yellow [:a.f4.avenir.near-black.hover-washed-yellow {:href "https://lambdaschool.com/courses/full-stack-web-development"} "Lambda School Fullstack Web Development track."]]]
            [:div.flex.flex-row.flex-wrap.justify-center.hover-animate
@@ -12,3 +12,5 @@
             [:p.ma2.avenir.grow.hover-animate.hover-light-green.ml1.hover-bg-near-black.bg-near-white.w-60.br3.pa2.dark-pink.w-50-m "But I will try and make reading it worth your time!"]
             ]]
           ]))
+
+(def valsfsfj "sfsf")
