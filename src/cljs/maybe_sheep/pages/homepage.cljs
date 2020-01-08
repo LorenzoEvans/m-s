515dc90a4b8c3b287c1d30a236b8fe4acceacb89
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-08 18:51:13 -0500

    should be writing buuuuut...

--- src/cljs/maybe_sheep/pages/homepage.cljs
+++ src/cljs/maybe_sheep/pages/homepage.cljs
@@ -16,28 +16,17 @@
 (defn about-anchor [] [:a.w-90.mono-gf {:class link-styling-home :href "#/about"} "About"])
 (defn portfolio-anchor [] [:a.w-90.mono-gf {:class link-styling-home :href "http://www.lorenzoevans.me/"} "Portfolio"])
 
-; (defn home-page []
-;       (fn []
-;        [:div.flex.flex-row.flex-column-m.justify-around-m.b--near-black.bw2.bg-image.vh-100
-;         [:nav.w-100.items-center.flex.flex-column.justify-around.self-center.mt2.mb2.flex-column-m.justify-between-m.h-100-m]
-;         [posts-anchor]
-;         [portfolio-anchor]
-;         [github-anchor]
-;         [twitter-anchor]
-;         [about-anchor]
-;         [misc-anchor]
-;         [:h1.justify-center.w-50.w-90-m.flex-column-m.bw2.avenir.dark-gray.f1.ba.b--near-black.bw1.shadow-3.f2-m.tc.self-center 
-;          [:div.w-50.tracked-mega.h3-m [:span.f1.tracked-mega.f2-m "Maybe"] [:span.f1.tracked-mega.f2-m "Sheeps"]]
-;          [:div.br3.bw2.ba.b--near-black.tc.avenir.near-white.bg-near-black.w-50.f4.pa3.lh-copy.fw2.vh-50.self-center.w-75-m.shadow-3.dn-m.justify-between
-;           [:div.fw3.bb.bt.bw1.ma2.b--near-white. "A blog dedicated to *Functional Programming, and other niche and weird *tech things of my choosing."]
-;           [:div.fw3.bb.bw1.ma2.b--near-white "Yes, the name is from the 'Maybe Not' Rich Hickey talk."]
-;           [:div.f4.fw1.ba.bw1.ma2.b--near-white "* Manufactured on equipment that processes OOP: may contain imperative code & mutable state"]
-;           [:div.f4.fw1.ba.bw1.ma2.b--near-white " * may contain non-tech related things because this is my blog and my code is law here. "]]]]))
 
+(def outer-home-page (new-string "flex.flex-row.flex-column-m.justify-around-m.b--near-black.bw2.bg-near-black.vh-100.mono-gf.bg-image.items-center"))
+(def home-page-nav (new-string "w-30.items-center.flex.flex-column.justify-around.self-center.mt2.mb2.flex-column-m.justify-between-m.h-100-m.ma2.vh-75"))
+(def outer-title (new-string "w-100.bg-image-6.tracked-mega.h3-m.bg-near-white.flex.flex-column.content-center.mt4"))
+(def title-synths (new-string ".m-sub.f1.tracked-mega.f2-m.fw1.self-center.near-white.bb.bw1.b--moon-gray.light-blue"))
+(def title-for (new-string ".m-sub.f1.tracked-mega.f2-m.fw1.self-center.near-white.bb.bw1.b--moon-gray.hot-pink"))
+(def title-compilers (new-string ".m-sub.f1.tracked-mega.f2-m.fw1.self-center.near-white.bb.bw1.b--moon-gray.navy"))
 (defn home-page []
 (fn []
-  [:div.flex.flex-row.flex-column-m.justify-around-m.b--near-black.bw2.bg-near-black.vh-100.mono-gf.bg-image.items-center
-   [:nav.w-30.items-center.flex.flex-column.justify-around.self-center.mt2.mb2.flex-column-m.justify-between-m.h-100-m.ma2.vh-75
+  [:div {:class outer-home-page}
+   [:nav {:class home-page-nav}
     [posts-anchor]
     [portfolio-anchor]
     [github-anchor]
@@ -45,7 +34,7 @@
     [about-anchor]
     [misc-anchor]
     [arena-anchor]]
-   [:div#p-gram.w-100.bg-image-6.tracked-mega.h3-m.bg-near-white.flex.flex-column.content-center.mt4
-    [:span#n-skew.m-sub.f1.tracked-mega.f2-m.fw1.self-center.near-white.bb.bw1.b--moon-gray.light-blue "s[Y]nths"] 
-    [:span#n-skew.m-sub.f1.tracked-mega.f2-m.fw1.self-center.near-white.bb.bw1.b--near-black.hot-pink "f0r"] 
-    [:span#n-skew.m-sub.f1.tracked-mega.f2-m.fw1.b.self-center.near-white.bb.bw1.b--light-pink.navy "comp[I]lers"]]]))
+   [:div#p-gram {:class outer-title}
+    [:span#n-skew {:class title-synths} "s[Y]nths"] 
+    [:span#n-skew {:class title-for} "f0r"] 
+    [:span#n-skew {:class title-compilers} "comp[I]lers"]]]))
--- src/cljs/maybe_sheep/pages/misc.cljs
+++ src/cljs/maybe_sheep/pages/misc.cljs
@@ -7,19 +7,19 @@
 ; we need to randomly grab an index
 
 (defn misc-page []
-    [:main.pa2
-     [:section.f1.avenir "Things that interest me"]
-     [:div.vh-100.flex.flex-column.flex-wrap.justify-evenly 
+    [:main.flex.flex-column.justify-center
+     [:section.f1.bb.b--black.bw2.w-100.h4.tc.sub-m "Things that interest me"]
+     [:div.vh-100.flex.flex-column.flex-wrap.justify-around
       (for [item misc-items]
-       [:div.h-30.w-auto.pa2.grow.db.ma2.ba.bw1.b--black {:class (:bg item)}
-        [:section.f2.avenir.w-50 (item :title)]
-        [:section.pa2.tc (item :type)]
-        [:a.no-underline.avenir {:class link-styling-x :href (item :link)} (item :title)]
-        [:blockquote.pa2.f2.bw1.avenir.w-auto 
-          [:div.f4.w-20 "Tags"]
+       [:div.grow.ma2.ba.bw1.b--black.bw2.shadow-3
+        [:section.f2.avenir.sub-m.tc.ba.bw2.b--light-gray (item :title)]
+        [:div.pa2.f2.bw1.avenir.w-100.flex.flex-column
+         [:a.no-underline.avenir.near-black.hover-animate.grow.hover-navy.self-center {:href (item :link)} "Link"]
+          [:div.f4.w-20.tc.self-center "Tags"]
          (for [tag (:tags item)]
-          [:span.ma1.f5.pa2.ba.bw1.b--black.shadow-3.flex.flex-row.flex-wrap.tc {:class (cond 
+          [:span.ma1.f5.pa2.ba.bw1.b--black.shadow-3.tc.w-auto {:class (cond 
                                      (= tag "application") "bg-washed-red"
                                      (= tag "minimalist")  "bg-washed-green"
                                      (= tag "art") "bg-washed-blue"
-                                     :else "bg-moon-gray")} tag])]])]])
+                                     :else "bg-moon-gray")} tag])
+          ]])]])
