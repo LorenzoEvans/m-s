6665e2047b2b8be0d299d17d3e444d2a95124ba2
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-11 21:07:03 -0700

    Alright, I'm beat, gotta call it wraps.

--- src/cljs/maybe_sheep/pages/posts.cljs
+++ src/cljs/maybe_sheep/pages/posts.cljs
@@ -7,11 +7,11 @@
 
 (defn posts-page []
   (fn []
-      [:span.w-100.flex.flex-column.justify-center.overflow-scroll.content-center.items-center.bw2.b--black.bg-image-2.vh-100
+      [:span.w-100.overflow-scroll.bw2.b--black.bg-image-2.vh-100 {:class styles/post-page-container}
           [:h1.w-100.tc.avenir.bw2.avenir.moon-gray.f1.b--near-black.bw1.f2-m.tc.self-center.flex.flex-column.justify-center "Maybe Posts"
               (for [item content-list]
                   (let [kw (first item) {:keys [title url prev]} (second item)]
                           ^{:key kw}
-                          [:div.w-90.flex.flex-row.justify-center.ma2.shadow-3.ba.bw2.f3.bg-near-black.self-center
+                          [:div.w-90.ma2.shadow-3.ba.bw2.f3.bg-near-black {:class styles/flex-row-center}
                            [:a.avenir.link.dim.near-white.truncate {:href url} title]]))]
             [:a.avenir {:class styles/link-styling-x :href "#/"} "Home"]]))
--- src/cljs/maybe_sheep/styles.cljs
+++ src/cljs/maybe_sheep/styles.cljs
@@ -7,11 +7,12 @@
 (def link-styling-x (new-string "pa2.grow.bw1.w-40.ma1.f3.near-black.bg-near-white.w4.w-75-m.hover-bg-dark-gray.bb.bw3.br1.hover-washed-green.b--near-black.shadow-3.flex.items-center.justify-center"))
 
 
-
-
 (def outer-home-page (new-string "flex.flex-column.flex-column-m.justify-between.justify-around-m.b--near-black.bw2.bg-near-black.vh-100.mono-gf.bg-image.items-center"))
 (def home-page-nav (new-string "w-100.items-center.flex.flex-row.justify-between.self-center.mb2.flex-column-m.justify-between-m.vh-100-m.vh-25.bt.bw3.b--washed-red.shadow3.h-100.bg-washed-red.bb"))
 (def outer-title (new-string "w-100.bg-image-6.tracked-mega.h3-m.bg-near-white.flex.flex-column.content-center.mb4"))
 (def title-synths (new-string ".m-sub.f1.tracked-mega.f2-m.fw1.self-center.near-white.bb.bw1.b--moon-gray.light-blue"))
 (def title-for (new-string ".m-sub.f1.tracked-mega.f2-m.fw1.self-center.near-white.bb.bw1.b--moon-gray.hot-pink"))
 (def title-compilers (new-string ".m-sub.f1.tracked-mega.f2-m.fw1.self-center.near-white.bb.bw1.b--moon-gray.navy"))
+
+(def post-page-container "flex flex-column justify-center content-center items-center w-100 overflow-scroll bw2 b--black bg-image-2 vh-100")
+(def flex-row-center "flex flex-row justify-center self-center w-100 overflow-scroll bw2 b--black bg-image-2vh-100")
